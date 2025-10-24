51f5774ccad09022130d1df72f17bb9d03edc4eb
KamalK1ng <nichalosking@gmail.com>
2025-10-24 18:21:14 +0100

    initial commit for api

---
+++ powershell.ps1
@@ -0,0 +1,7 @@
+# git config --global user.name "KamalK1ng"
+# git config --global user.email "nichalosking@gmail.com"
+
+git init
+git add .
+git commit -m "initial commit for api"
+git branch -M main
